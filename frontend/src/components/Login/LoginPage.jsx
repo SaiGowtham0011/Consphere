71088979042fe6878d9982ca8b48020bc9f05d24
SaiGowtham0011 <gowthamnama38@gmail.com>
2026-06-01 17:09:15 +0530

    Desinged Login Page

--- frontend/src/components/Login/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage.jsx
@@ -1,22 +1,30 @@
 import {Link} from 'react-router-dom'
 import './LoginPage.css'
+import banner from '../../assets/loginbanner.png'
 
 function Login(){
     return(
         <div className='container'>
-            <div className='login-box'>
-                <h3>Login</h3>
-                <div className='login-input'>
-                    <input placeholder="enter username"></input>
-                    <input placeholder="enter password"></input>
+            <div className='left-side'>
+                <div className='login-box'>
+                    <h1 className='heading'>Login</h1>
+                    <div className='user-input'>
+                        <input id='user-box' placeholder="enter username"></input>
+                    </div>
+                    <div className='password-input'>
+                        <input id='pass-box' placeholder="enter password"></input>
+                    </div>
+                    <div className='login-btn'>
+                        <button>Login</button>
+                    </div>
+                    <div className='newAcc'>
+                        <p>Didn't have an account ?</p>
+                        <Link to="/signin">Sign In</Link>
+                    </div>
                 </div>
-                <div className='login-btn'>
-                    <button>Login</button>
-                </div>
-                <div className='newAcc'>
-                    <p>Didn't have an account ?</p>
-                    <Link to="/signin">Sign In</Link>
-                </div>
+            </div>
+            <div className='right-side'>
+                <img src={banner} alt='Login Banner'></img>
             </div>
         </div>
     )
